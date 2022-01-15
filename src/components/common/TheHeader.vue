b8f08c28c3b30de227f4d43ec4a00594e2d38aab
Your Name <you@example.com>
2022-01-15 21:52:41 +0700

    install package

--- src/components/common/TheHeader.vue
+++ src/components/common/TheHeader.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-      <h1>Header</h1>
+  <div class="wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h1>Header</h1>
+
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
