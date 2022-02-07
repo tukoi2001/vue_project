5480553da4212a25006887f73fcc22e7fa93fa04
Your Name <you@example.com>
2022-02-08 00:11:14 +0700

    update delete item and clear all item wishlist page

--- src/components/wishlist/MainWishlist.vue
+++ src/components/wishlist/MainWishlist.vue
@@ -17,12 +17,14 @@
             <tbody>
               <tr v-for="(item, index) in listProducts" :key="index">
                 <td class="pro-thumbnail">
-                  <a href=""
-                    ><img :src="item.image" alt="Product"
-                  /></a>
+                  <a href=""><img :src="item.image" alt="Product" /></a>
                 </td>
-                <td class="pro-title"><a href="">{{ item.title }}</a></td>
-                <td class="pro-price"><span>$ {{ item.price }}</span></td>
+                <td class="pro-title">
+                  <a href="">{{ item.title }}</a>
+                </td>
+                <td class="pro-price">
+                  <span>$ {{ item.price }}</span>
+                </td>
                 <td class="pro-quantity">
                   <div class="pro-qty">
                     <div class="count-input-block">
@@ -30,14 +32,29 @@
                         type="number"
                         class="form-control text-center"
                         v-model="item.value"
-                        min="1" max="20"
+                        min="1"
+                        max="20"
                       />
                     </div>
                   </div>
                 </td>
-                <td class="pro-subtotal"><span>$ {{ totalPrice(item.price, item.value) }}</span></td>
+                <td class="pro-subtotal">
+                  <span>$ {{ totalPrice(item.price, item.value) }}</span>
+                </td>
                 <td class="pro-remove">
-                  <a href="#"><b-icon class="table-icon" icon="trash-fill"></b-icon></a>
+                  <a href="" @click.prevent="deleteProduct(item.id)"
+                    ><b-icon class="table-icon" icon="trash-fill"></b-icon
+                  ></a>
+                </td>
+              </tr>
+              <tr v-if="listProducts.length === 0">
+                <td colspan="6" class="text-danger">There isn't any item!</td>
+              </tr>
+              <tr v-else>
+                <td colspan="6" class="last-row">
+                  <div class="text-start">
+                    <button @click="clearAllItems" type="button" class="btn btn-primary">Clear</button>
+                  </div>
                 </td>
               </tr>
             </tbody>
@@ -52,39 +69,49 @@
 export default {
   name: "MainWishlist",
   data() {
-      return {
-          listProducts: [
-              {
-                  image: require('@/assets/images/products/kinh-te/bao-cao-tai-chinh-duoi-goc-nhin-cua-warren-buffett-tai-ban/bao-cao-tai-chinh-duoi-goc-nhin-cua-warren-buffett-tai-ban_1.jpg'),
-                  title: 'Rinosin Glasses',
-                  value:  1,
-                  price: '395.00',
-                  total: '395.00',
-              },
-              {
-                  image: require('@/assets/images/products/kinh-te/bao-cao-tai-chinh-duoi-goc-nhin-cua-warren-buffett-tai-ban/bao-cao-tai-chinh-duoi-goc-nhin-cua-warren-buffett-tai-ban_1.jpg'),
-                  title: 'Rinosin Glasses',
-                  value:  1,
-                  price: '395.00',
-                  total: '395.00',
-              },
-              {
-                  image: require('@/assets/images/products/kinh-te/bao-cao-tai-chinh-duoi-goc-nhin-cua-warren-buffett-tai-ban/bao-cao-tai-chinh-duoi-goc-nhin-cua-warren-buffett-tai-ban_1.jpg'),
-                  title: 'Rinosin Glasses',
-                  value:  1,
-                  price: '395.00',
-                  total: '395.00',
-              }
-          ]
-      }
+    return {
+      listProducts: [
+        {
+          id: 0,
+          image: require("@/assets/images/products/kinh-te/bao-cao-tai-chinh-duoi-goc-nhin-cua-warren-buffett-tai-ban/bao-cao-tai-chinh-duoi-goc-nhin-cua-warren-buffett-tai-ban_1.jpg"),
+          title: "Rinosin Glasses",
+          value: 1,
+          price: "395.00",
+        },
+        {
+          id: 1,
+          image: require("@/assets/images/products/kinh-te/bao-cao-tai-chinh-duoi-goc-nhin-cua-warren-buffett-tai-ban/bao-cao-tai-chinh-duoi-goc-nhin-cua-warren-buffett-tai-ban_1.jpg"),
+          title: "Rinosin Glasses",
+          value: 1,
+          price: "395.00",
+        },
+        {
+          id: 2,
+          image: require("@/assets/images/products/kinh-te/bao-cao-tai-chinh-duoi-goc-nhin-cua-warren-buffett-tai-ban/bao-cao-tai-chinh-duoi-goc-nhin-cua-warren-buffett-tai-ban_1.jpg"),
+          title: "Rinosin Glasses",
+          value: 1,
+          price: "395.00",
+        },
+      ],
+    };
   },
   methods: {
-      totalPrice(price, num) {
-        const number = Number(num);
-        const result = price * number;
-        return result;
-      },
-  }
+    totalPrice(price, num) {
+      const number = Number(num);
+      const result = price * number;
+      return result;
+    },
+    deleteProduct(index) {
+      if (this.listProducts.length === 1) {
+        this.listProducts.splice(0, 1);
+      } else {
+        this.listProducts.splice(index, 1);
+      }
+    },
+    clearAllItems() {
+        this.listProducts.splice(0, this.listProducts.length);
+    }
+  },
 };
 </script>
 
